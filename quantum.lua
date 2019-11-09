c3a64d4173200b5810b3110a0e23f7d2ff755fa7
Basilio German <basiliogerman@gmail.com>
2019-11-09 03:32:35 -0600

    Updated metatable remove first on finalize

--- quantum.lua
+++ quantum.lua
@@ -346,15 +346,16 @@ local function finalizeEntangledObject(event)
 	
 	display.remove(lightObject.normalObject)
 	
+	-- First remove metatable
+	setmetatable(lightObject, lightObject._superMeta)
+	lightObject._superMeta = nil
+	
 	lightObject.normalObject = nil
 	lightObject.fillProxy = nil
 	lightObject.entangleFunctions = nil
 	lightObject.viewRotation = nil
 	
 	lightObject._camera = nil
-	
-	setmetatable(lightObject, lightObject._superMeta)
-	lightObject._superMeta = nil
 end
 
 local function entangleObject(lightObject, entangleFunctions) -- Basic light object principle, where we make object pairs in different worlds (diffuse & normal)
